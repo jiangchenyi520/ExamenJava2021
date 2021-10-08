0e1b7c2fb710a9b9b8de0dcb139328afec6232a2
“Chenyi“ <jiangchenyi52@gmail.com>
2021-10-08 16:54:05 +0200

    get and set , const

--- CalidadAire/src/main/java/com/example/calidadaire/CalidadAire.java
+++ CalidadAire/src/main/java/com/example/calidadaire/CalidadAire.java
@@ -5,29 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalidadAire {
+
     // Hora local Gandia Epoch del 7.10.2021 de las 8:00 am a las 17:00 pm
-    static long FECHA[] = {1633586400000L, 1633590000000L, 1633593600000L, 1633597200000L, 1633600800000L, 1633604400000L, 1633608000000L, 1633611600000L, 1633615200000L, 1633618800000L};
+    static long FECHA[]={1633586400000L, 1633590000000L, 1633593600000L, 1633597200000L, 1633600800000L, 1633604400000L, 1633608000000L,1633611600000L, 1633615200000L, 1633618800000L};
 
     // Mediciones de la concentración de CO2 (calidad del aire en partículas por millón, ppm) en las aulas
     static int LELECTRONICA[] = {500, 650, 700, 850, 875, 600, 550, 650, 850, 600};
     static int LTELEMATICA[] = {450, 500, 675, 875, 850, 700, 750, 880, 700, 600};
     static int AULA9[] = {450, 500, 550, 650, 700, 800, 750, 600, 650, 600};
-
     //En la clase CalidadAire, crea una variable que lea los datos representados en las tres constantes y los almacene en una lista de objetos de tipo MedidaCO2.
+
     public static void main(String[] args){
 
-        //Crea una lista para despues almacenar
-        List<MedidaCO2> ListaNueva= new ArrayList<>();
+        List<MedidaCO2> Hello = new ArrayList<>();
 
-        for(int i =0; i<FECHA.length; i++){
-            MedidaCO2 a = new MedidaCO2((int) FECHA[i], LELECTRONICA[i], LTELEMATICA[i],AULA9[i]);
-            ListaNueva.add(a);
-            System.out.println(a);
+        for (int i = 0; i< FECHA.length; i++){
+
+            MedidaCO2 m = new MedidaCO2(FECHA[i], LELECTRONICA[i], LTELEMATICA[i],AULA9[i]);
+            Hello.add(m);
         }
+        System.out.println(Hello.toString());
+    }}
 
-    }
-
-}
-
-
-}
--- CalidadAire/src/main/java/com/example/calidadaire/MedidaCO2.java
+++ CalidadAire/src/main/java/com/example/calidadaire/MedidaCO2.java
@@ -1,7 +1,5 @@
 package com.example.calidadaire;
 
-import java.util.List;
-
 /*
 Tiempo en milisegundos en que se realizó la lectura.
 o    Concentración en ppm de CO2 en el Laboratorio de Electrónica.
@@ -10,47 +8,48 @@ o    Concentración en ppm de CO2 en el Aula 9.
 Crea el método constructor de la clase los métodos Get y Set para acceder a estos atributos.
  */
 public class MedidaCO2 {
-    private int tiempo;
-    private double CO2Elect;
-    private double CO2Telem;
-    private double Aula9;
+    private long fecha;
+    private int CO2Elect;
+    private int CO2Telem;
+    private int Aula9;
 
-    public int getTiempo() {
-        return tiempo;
+    public long getFecha() {
+        return fecha;
     }
 
-    public void setTiempo(int tiempo) {
-        this.tiempo = tiempo;
+    public void setFecha(long fecha) {
+        this.fecha = fecha;
     }
 
-    public double getCO2Elect() {
+    public int getCO2Elect() {
         return CO2Elect;
     }
 
-    public void setCO2Elect(double CO2Elect) {
+    public void setCO2Elect(int CO2Elect) {
         this.CO2Elect = CO2Elect;
     }
 
-    public double getCO2Telem() {
+    public int getCO2Telem() {
         return CO2Telem;
     }
 
-    public void setCO2Telem(double CO2Telem) {
+    public void setCO2Telem(int CO2Telem) {
         this.CO2Telem = CO2Telem;
     }
 
-    public double getAula9() {
+    public int getAula9() {
         return Aula9;
     }
 
-    public void setAula9(double aula9) {
+    public void setAula9(int aula9) {
         Aula9 = aula9;
     }
 
-    public MedidaCO2(int tiempo, double CO2Elect, double CO2Telem, double aula9) {
-        this.tiempo = tiempo;
+    public MedidaCO2(long fecha, int CO2Elect, int CO2Telem, int aula9) {
+        this.fecha = fecha;
         this.CO2Elect = CO2Elect;
         this.CO2Telem = CO2Telem;
         Aula9 = aula9;
     }
 }
+
